a0520084bf185883be402c6e5fc93ed65fe812d1
Esa Torkkila <esa.torkkila@gmail.com>
2022-05-19 12:00:58 +0300

    Counter practice with callback function.

---
+++ src/Practices/State.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./state.css"
+
+export default function State(name) {
+    const [isImportant, setIsImportant] = React.useState("Yes")
+    /**
+     * Challenge: 
+     * 1. Create a function called `handleClick` that runs
+     *    setIsImportant("No")
+     * 2. add a click event listener to the div.state--value
+     *    that runs `handleClick` when the div is clicked.
+     */
+    alert("State")
+    function handleClick() {
+        isImportant == "Yes" ? setIsImportant("No") :  setIsImportant("Yes")
+    }
+    
+    return (
+        <div className="state">
+            <h1 className="state--title">Is state important to know?</h1>
+            <div className="state--value" onClick={handleClick}>
+                <h1>{isImportant}</h1>
+            </div>
+        </div>
+    )
+}
