5a8ceb7c788773f9ea661413b578973af58f7197
Esa Torkkila <esa.torkkila@gmail.com>
2022-05-22 13:06:57 +0300

    Raising state from child components

--- src/Practices/Practices.js
+++ src/Practices/Practices.js
@@ -1,13 +1,14 @@
 import React, {useState} from 'react';
 /*import State from './State';
 import ThingsArray from "./ThingsArray"
-import Counter from "./Counter"*/
-import Objects from "./Objects"
+import Counter from "./Counter"
+import Objects from "./Objects"*/
+import PassData from "./PassData"
 
 export default function Practices() {
     return (
         <>
-            <Objects />
+            <PassData />
         </>
     )
 }
