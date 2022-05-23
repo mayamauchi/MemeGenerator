4031a55a02dc35a5928b7e138d0e6ffce381a51e
Esa Torkkila <esa.torkkila@gmail.com>
2022-05-23 14:29:37 +0300

    Boxes challenge unified state

--- src/Practices/BoxesChallenge.js
+++ src/Practices/BoxesChallenge.js
@@ -4,36 +4,28 @@ import "./boxeschallenge.css"
 import Box from "./Components/Box"
 
 export default function BoxesChallenge(props) {
-    const [squares, setSquares] = useState(boxes)
+    const [squares, setSquares] = React.useState(boxes)
     
-/*    const squareElements = squares.map(square => (
-        <Box 
-            key={square.id}
-            on={square.on}
-            name="Esa"
-        />
-    ))*/
-    const squareElements = squares.map(square => {
-        console.log(square)
-        console.log(square.on)
-        return (
-            <Box 
-                key={square.id}
-                on={square.on}
-                name="Esa"
-            />
-        )
-    })
     /**
-     * Challenge part 2:
-     * 1. Create a separate component called "Box" and
-     *    replace the `div` above with our <Box /> components
-     * 2. Pass the Box component a prop called `on` with the
-     *    value of the same name from the `boxes` objects
-     * 3. In the Box component, apply dynamic styles to determine
-     *    the backgroundColor of the box. If it's `on`, set the
-     *    backgroundColor to "#222222". If off, set it to "none"
+     * Challenge: Create a toggle() function that logs
+     * "clicked!" to the console
+     * 
+     * Pass that function down to each of the Box components
+     * and set it up so when they get clicked it runs the function
      */
+    
+    function toggle() {
+        console.log(squares)
+
+    }
+
+    const squareElements = squares.map(square => (
+        <Box 
+            key={square.id} 
+            on={square.on} 
+            onClick={toggle}
+        />
+    ))
     
     return (
         <main>
