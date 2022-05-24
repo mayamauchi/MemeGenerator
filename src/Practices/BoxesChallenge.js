8c633f54d3dc1c28e8eb8c93c041ac334465e213
Esa Torkkila <esa.torkkila@gmail.com>
2022-05-24 08:17:59 +0300

    Boxes challenge updating object in state array with closure

--- src/Practices/BoxesChallenge.js
+++ src/Practices/BoxesChallenge.js
@@ -7,37 +7,18 @@ export default function BoxesChallenge(props) {
     const [squares, setSquares] = React.useState(boxes)
     
     function toggle(id) {
-        /**
-         * Challenge: use setSquares to update the
-         * correct square in the array.
-         * 
-         * Make sure not to directly modify state!
-         * 
-         * Hint: look back at the lesson on updating arrays
-         * in state if you need a reminder on how to do this
-         */
         setSquares(prevSquares => {
-            return prevSquares.map(square => {
-                return square.id == id ? {...square, on: !square.on } : square
-            }
-        )})
-/*
-        setSquares(prevSquares => {
-            return prevSquares.map(square => {return 1}
-        )})
-        */
-/*        let index = id -1
-        let s = squares.slice(0,index)
-        let i = squares.slice(index,index+1)
-        let e = squares.slice(index+1,squares.length) 
-*/
+            return prevSquares.map((square) => {
+                return square.id === id ? {...square, on: !square.on} : square
+            })
+        })
     }
+    
     const squareElements = squares.map(square => (
         <Box 
             key={square.id} 
-            id={square.id}
             on={square.on} 
-            toggle={toggle}
+            toggle={() => toggle(square.id)}
         />
     ))
     
