a50320972858d4d4e594a927998a137e80bf7bb5
Esa Torkkila <esa.torkkila@gmail.com>
2022-05-27 18:17:44 +0300

    Forms: sign up form practice: Bob's solution

--- src/Practices/SignUpForm.js
+++ src/Practices/SignUpForm.js
@@ -2,6 +2,12 @@ import React from "react"
 import "./Components/signupform.css"
 
 export default function App() {
+    const [formData, setFormData] = React.useState({
+        email: "",
+        password: "",
+        passwordConfirm: "",
+        joinedNewsletter: true
+    })
     
     /**
      * Challenge: Connect the form to local state
@@ -13,46 +19,34 @@ export default function App() {
      * 3. When the user clicks "Sign up", check if the 
      *    password & confirmation match each other. If
      *    so, log "Successfully signed up" to the console.
-     *    If not, log "passwords to not match" to the console.
+     *    If not, log "passwords do not match" to the console.
      * 4. Also when submitting the form, if the person checked
      *    the "newsletter" checkbox, log "Thanks for signing
      *    up for our newsletter!" to the console.
      */
-    const [formData, setFormData] = React.useState(
-        {
-            email: "",
-            pswd: "",
-            pswdConf: "",
-            join: false
-        }
-    )
-    console.log(formData.email, formData.pswd, formData.pswdConf, formData.join)
-    function handleSubmit(event) {
-        event.preventDefault()
-        console.log("handleSubmit")
-
-        if (formData.pswd === formData.pswdConf) {
-            console.log("Successfully signed up")
-            if (formData.join) 
-                console.log("Thanks for signing up for our newsletter!")
-        }
-        else
-            console.log("Passwords do not match")
-
-    
-    }
     
     function handleChange(event) {
-        const {type, name, value, checked} = event.target
-
-        setFormData(prevData => {
-            return {
-                ...prevData,
-                [name]: type==="checkbox" ? checked : value
-            }
-        })
+        const {name, value, type, checked} = event.target
+        setFormData(prevFormData => ({
+            ...prevFormData,
+            [name]: type === "checkbox" ? checked : value
+        }))
     }
-
+    
+    function handleSubmit(event) {
+        event.preventDefault()
+        if(formData.password === formData.passwordConfirm) {
+            console.log("Successfully signed up")
+        } else {
+            console.log("Passwords do not match")
+            return
+        }
+        
+        if(formData.joinedNewsletter) {
+            console.log("Thanks for signing up for our newsletter!")
+        }
+    }
+    
     return (
         <div className="form-container">
             <form className="form" onSubmit={handleSubmit}>
@@ -60,40 +54,39 @@ export default function App() {
                     type="email" 
                     placeholder="Email address"
                     className="form--input"
-                    onChange={handleChange}
                     name="email"
+                    onChange={handleChange}
                     value={formData.email}
                 />
                 <input 
                     type="password" 
                     placeholder="Password"
                     className="form--input"
+                    name="password"
                     onChange={handleChange}
-                    name="pswd"
-                    value={formData.pswd}
+                    value={formData.password}
                 />
                 <input 
                     type="password" 
                     placeholder="Confirm password"
                     className="form--input"
+                    name="passwordConfirm"
                     onChange={handleChange}
-                    name="pswdConf"
-                    value={formData.pswdConf}
+                    value={formData.passwordConfirm}
                 />
                 
                 <div className="form--marketing">
                     <input
                         id="okayToEmail"
                         type="checkbox"
+                        name="joinedNewsletter"
                         onChange={handleChange}
-                        name="join"
-                        checked= {formData.join ? 'checked' : false}
+                        checked={formData.joinedNewsletter}
                     />
                     <label htmlFor="okayToEmail">I want to join the newsletter</label>
                 </div>
                 <button 
                     className="form--submit"
-                    onClick={handleSubmit}
                 >
                     Sign up
                 </button>
