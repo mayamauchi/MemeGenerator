c211884cf9ceaebdbf8bf2c158eace0ed105989d
Esa Torkkila <esa.torkkila@gmail.com>
2022-05-28 06:18:46 +0300

    UseEffect when to use dependencies

--- src/Practices/UseEffect.js
+++ src/Practices/UseEffect.js
@@ -2,30 +2,29 @@ import React, {useEffect} from "react"
 
 export default function UseEffect() {
     const [starWarsData, setStarWarsData] = React.useState({})
-    const [count, setCount] = React.useState(0)
+    const [count, setCount] = React.useState(1)
     
     /**
-     * Quiz:
-     * 1. What will happen if I put back our Star Wars API call
-     *    into the effect function?
+     * Challenge: Combine `count` with the request URL
+     * so pressing the "Get Next Character" button will
+     * get a new character from the Star Wars API.
+     * Remember: don't forget to consider the dependencies
+     * array!
      */
-    React.useEffect(function() {
-        fetch("https://swapi.dev/api/people/1")
-            .then(res => {
-                console.log("RES===")
-                console.log(res)
-                return res.json()
-                console.log("JSON===")
+    console.log(count)
 
-            })
-            .then(data => console.log(data))
-    }, [starWarsData, count])
+    React.useEffect(function() {
+        console.log("Effect ran")
+        fetch(`https://swapi.dev/api/people/${count}`)
+            .then(res => res.json())
+            .then(data => setStarWarsData(data))
+    }, [count])
     
     return (
         <div>
-            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
             <h2>The count is {count}</h2>
-            <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Get Next Character</button>
+            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
         </div>
     )
 }
